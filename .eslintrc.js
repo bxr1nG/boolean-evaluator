05c5efba6babee16a3bb6479371f4790dbc74a2b
Daniil Voshchuk <vochshukdaniil@gmail.com>
2023-02-18 20:44:33 +0300

    chore: add prettier and eslint

---
+++ .eslintrc.js
@@ -0,0 +1,71 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+        node: true
+    },
+    extends: [
+        "eslint:recommended",
+        "plugin:import/errors",
+        "plugin:import/warnings",
+        "prettier"
+    ],
+    overrides: [
+        {
+            files: ["*.ts", "*.tsx"],
+            extends: [
+                "plugin:@typescript-eslint/recommended",
+                "plugin:@typescript-eslint/recommended-requiring-type-checking"
+            ],
+            parserOptions: {
+                project: ["./configs/tsconfig.base.json"],
+                tsconfigRootDir: __dirname
+            }
+        }
+    ],
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module"
+    },
+    plugins: ["@typescript-eslint", "import", "prettier"],
+    rules: {
+        "prettier/prettier": "error",
+        "@typescript-eslint/no-unused-vars": "error",
+        "@typescript-eslint/consistent-type-imports": "error",
+        "import/order": [
+            "error",
+            {
+                groups: [
+                    "type",
+                    "builtin",
+                    "external",
+                    "internal",
+                    "parent",
+                    "index",
+                    "sibling",
+                    "object"
+                ],
+                pathGroups: [
+                    {
+                        pattern: "~/types/**",
+                        group: "parent",
+                        position: "before"
+                    },
+                    {
+                        pattern: "~/**",
+                        group: "parent",
+                        position: "before"
+                    },
+                    {
+                        pattern: "./**",
+                        group: "sibling",
+                        position: "before"
+                    }
+                ],
+                "newlines-between": "always",
+                distinctGroup: false
+            }
+        ]
+    }
+};
