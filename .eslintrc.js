d68a8192a604882757976977223199e92568f90d
bxr1nG <vochshukdaniil@gmail.com>
2023-02-21 15:19:31 +0300

    chore(.eslintrc.js): add jest plugin for ESLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -35,9 +35,14 @@ module.exports = {
         ecmaVersion: "latest",
         sourceType: "module"
     },
-    plugins: ["@typescript-eslint", "import", "prettier"],
+    plugins: ["@typescript-eslint", "import", "prettier", "jest"],
     rules: {
         "prettier/prettier": "error",
+        "jest/no-disabled-tests": "warn",
+        "jest/no-focused-tests": "error",
+        "jest/no-identical-title": "error",
+        "jest/prefer-to-have-length": "warn",
+        "jest/valid-expect": "error",
         "@typescript-eslint/no-unused-vars": "error",
         "@typescript-eslint/consistent-type-imports": "error",
         "import/order": [
